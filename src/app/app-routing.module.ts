2a82997385602e2f809427228af9e692159fe941
Ashe <asheya.yuson@usjr.edu.ph>
2022-12-09 13:56:00 +0800

    added reports screen

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { DataTableComponent } from './data-table/data-table.component';
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
 import { ForgetPasswordComponent } from './forget-password/forget-password.component';
 import { UserManagementComponent } from './user-management/user-management.component';
+import { ReportsComponent } from './reports/reports.component';
 
 
 
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'datatable', component: DataTableComponent},
   { path: 'forget-password', component: ForgetPasswordComponent},
   { path: 'user-management', component: UserManagementComponent},
+  { path: 'reports', component: ReportsComponent},
   
   
 ];
@@ -22,4 +24,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [ LoginComponent, DataTableComponent, ForgetPasswordComponent, UserManagementComponent]
+export const routingComponents = [ LoginComponent, DataTableComponent, ForgetPasswordComponent, UserManagementComponent, ReportsComponent]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,11 @@ import {MatTableModule} from '@angular/material/table';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DataTableComponent } from './data-table/data-table.component';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatDialogModule } from '@angular/material/dialog'
 import { MatSortModule } from '@angular/material/sort';
 import { NewTicketComponent } from './new-ticket/new-ticket.component';
-import { EditTicketComponent } from './edit-ticket/edit-ticket.component';
-import { ForgetPasswordComponent } from './forget-password/forget-password.component';
-import { UserManagementComponent } from './user-management/user-management.component';
-import { EditAddUserComponent } from './edit-add-user/edit-add-user.component';
+
 
 
 
@@ -22,12 +18,7 @@ import { EditAddUserComponent } from './edit-add-user/edit-add-user.component';
   declarations: [
     AppComponent,
     routingComponents,
-    DataTableComponent,
-    NewTicketComponent,
-    EditTicketComponent,
-    ForgetPasswordComponent,
-    UserManagementComponent,
-    EditAddUserComponent,
+    
   
   ],
   entryComponents: [NewTicketComponent],
